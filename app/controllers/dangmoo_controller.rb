344dbcfac78d9bd2d9bd5e8433b51d31fb2a5ab1
shinatatsu <shinatatsu040849@gmail.com>
2018-03-23 20:55:38 +0800

    use pg

--- app/controllers/dangmoo_controller.rb
+++ app/controllers/dangmoo_controller.rb
@@ -2,4 +2,7 @@ class DangmooController < ApplicationController
     def eat
         render plain: "痴痴痴"
     end
+    def webhook
+        head :ok
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/dangmoo/eat', to: 'dangmoo#eat'
+  post '/dangmoo/webhook', to: 'dangmoo#webhook'
 end
