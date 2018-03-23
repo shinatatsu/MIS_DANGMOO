d4e94b6b5c9b37c3bcf4c0ffa3ca621f00b4da90
shinatatsu <shinatatsu040849@gmail.com>
2018-03-23 20:40:46 +0800

    use pg

--- app/controllers/dangmoo_controller.rb
+++ app/controllers/dangmoo_controller.rb
@@ -2,25 +2,4 @@ class DangmooController < ApplicationController
     def eat
         render plain: "痴痴痴"
     end
-
-    def request_headers
-        render plain: request.headers.to_h.map{ |key, value|
-            key+":"+value.class.to_s}.sort.join("\n")
-    end
-
-    def response_headers
-        response.headers['5566'] = 'QQ'
-        render plain: response.headers.to_h.map{ |key, value|
-            "#{key}: #{value}"}.sort.join("\n")
-    end
-
-    def request_body
-        render plain: request.body
-    end
-
-    def show_response_body
-        puts "===設定前:#{response.body}==="
-        render plain:　"HIHIHIHIHI"
-        puts "==設定後:#{response.body}==="
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/dangmoo/eat', to: 'dangmoo#eat'
-  get '/dangmoo/sent_request', to: 'dangmoo#sent_request'
 end
