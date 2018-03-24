0d0a35bc6e637e175cac3283e6a0e5f5d3aa61f9
shinatatsu <shinatatsu040849@gmail.com>
2018-03-24 19:11:47 +0800

    reply message

--- app/controllers/dangmoo_controller.rb
+++ app/controllers/dangmoo_controller.rb
@@ -1,3 +1,4 @@
+require 'line/bot'
 class DangmooController < ApplicationController
     protect_from_forgery with: :null_session
     def eat
@@ -32,6 +33,25 @@ class DangmooController < ApplicationController
     end
 
     def webhook
+        #Line Bot API 物件初始化
+        client = Line::Bot::Client.new{ |config|
+            config.channel_secret = 'e55f897882eea3d7a0a3874600e1eeed'
+            config.channel_token = '2nuY4aNLo3XuxA7vQZyLaYnGg77cI5wRJj4IQq1KeB6jIbVGsEbDGDKxkQDjT
+            pdgj524NnFhl4m0aAa3y3gjtE9UcYSceuLs7PYhV8OKLr6Mla+civUW1VGnPtuxxtAnTt/BiFXb/1KqWNQF8+
+            zLDgdB04t89/1O/w1cDnyilFU='
+        }
+
+        #取得reply token
+        reply_token = params['event'][0]['replyToken']
+
+        #設定回復訊息
+        message = {
+            type: 'text',
+            text: '好哦～好哦～'
+        }   
+
+        # 傳送訊息
+        response = client.reply_message(reply_token, message)
         head :ok
     end
 
