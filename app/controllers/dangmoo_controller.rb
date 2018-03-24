7f82b44325f327cb80c0ba73bfccf731eff1f3ce
shinatatsu <shinatatsu040849@gmail.com>
2018-03-24 21:42:17 +0800

    reply message

--- app/controllers/dangmoo_controller.rb
+++ app/controllers/dangmoo_controller.rb
@@ -1,32 +1,6 @@
 require 'line/bot'
-class KamigoController < ApplicationController
+class DangmooController < ApplicationController
   protect_from_forgery with: :null_session
-
-  def webhook
-    # Line Bot API 物件初始化
-    client = Line::Bot::Client.new { |config|
-      config.channel_secret = 'e55f897882eea3d7a0a3874600e1eeed'
-      config.channel_token = '2nuY4aNLo3XuxA7vQZyLaYnGg77cI5wRJj4IQq1KeB6jIbVGsEbDGDKxkQDjTpdgj524NnFhl4m0aAa3y3gjtE9UcYSceuLs7PYhV8OKLr6Mla+civUW1VGnPtuxxtAnTt/BiFXb/1KqWNQF8+zLDgdB04t89/1O/w1cDnyilFU='
-    }
-    
-    # 取得 reply token
-        reply_token = params['events'][0]['replyToken']
-            p "=====reply_token"
-            p reply_token
-            p "====="
-
-    # 設定回覆訊息
-        message = {
-            type: 'text',
-            text: '好哦～好哦～'
-        }
-
-    # 傳送訊息
-        response = client.reply_message(reply_token, message)
-      
-    # 回應 200
-        head :ok
-    end 
 
     def eat
         # render plain: "痴痴痴"
@@ -58,4 +32,30 @@ class KamigoController < ApplicationController
         response = Net::HTTP.get(uri)
         render plain: response
     end
+
+    def webhook
+        # Line Bot API 物件初始化
+        client = Line::Bot::Client.new { |config|
+          config.channel_secret = 'e55f897882eea3d7a0a3874600e1eeed'
+          config.channel_token = '2nuY4aNLo3XuxA7vQZyLaYnGg77cI5wRJj4IQq1KeB6jIbVGsEbDGDKxkQDjTpdgj524NnFhl4m0aAa3y3gjtE9UcYSceuLs7PYhV8OKLr6Mla+civUW1VGnPtuxxtAnTt/BiFXb/1KqWNQF8+zLDgdB04t89/1O/w1cDnyilFU='
+        }
+        
+        # 取得 reply token
+            reply_token = params['events'][0]['replyToken']
+                p "=====reply_token"
+                p reply_token
+                p "====="
+    
+        # 設定回覆訊息
+            message = {
+                type: 'text',
+                text: '好哦～好哦～'
+            }
+    
+        # 傳送訊息
+            response = client.reply_message(reply_token, message)
+          
+        # 回應 200
+            head :ok
+        end 
 end
