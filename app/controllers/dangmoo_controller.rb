f69d5216e49f8d7a410668171c9237dc2a32f54d
shinatatsu <shinatatsu040849@gmail.com>
2018-03-24 15:27:43 +0800

    use pg

--- app/controllers/dangmoo_controller.rb
+++ app/controllers/dangmoo_controller.rb
@@ -2,6 +2,28 @@ class DangmooController < ApplicationController
     def eat
         # render plain: "痴痴痴"
     end
+
+    def request_headers
+        render plain: request.headers
+    end
+
+    def response_headers
+        response.headers['5566'] = 'QQ'
+        render plain: response.headers.to_h.map{ |key, value|
+            "#{key}: #{value}"
+        }.sort.join("\n")
+    end
+
+    def request_body
+        render plain: request.body
+    end
+    
+    def show_response_body
+        puts "===這是設定前的response.body:#{response.body}==="
+        render plain: "虎哇花哈哈哈"
+        puts "===這是設定後的response.body:#{response.body}==="
+    end
+
     def webhook
         head :ok
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/dangmoo/eat', to: 'dangmoo#eat'
+  get '/dangmoo/request_headers', to: 'dangmoo#request_headers'
+  get '/dangmoo/request_body', to: 'dangmoo#request_body'
+  get '/dangmoo/response_headers', to: 'dangmoo#response_headers'
+  get '/dangmoo/response_body', to: 'dangmoo#show_response_body'
   post '/dangmoo/webhook', to: 'dangmoo#webhook'
 end
