ae7f39ea7bc45fcaa35eae46f8235109fa7a724d
shinatatsu <shinatatsu040849@gmail.com>
2018-03-31 11:19:37 +0800

    use dangmoo

--- app/controllers/dangmoo_controller.rb
+++ app/controllers/dangmoo_controller.rb
@@ -4,7 +4,7 @@ class DangmooController < ApplicationController
 
 def webhook
     #學說話
-    room_id = room(received_text)
+    # room_id = room(received_text)
     reply_text = room(received_text)
 
     # #設定回復文字
@@ -23,27 +23,27 @@ def webhook
 end
 
 #擷取房號
-def room(received_text)
-    #如果開頭不是 ; 就跳出
-    return nil unless received_text[0] == ';'
+# def room(received_text)
+#     #如果開頭不是 ; 就跳出
+#     return nil unless received_text[0] == ';'
 
-    #擷取房號
-    roomid = received_text[1..-1]
-    # semicolon_index = received_text.index(";")
+#     #擷取房號
+#     roomid = received_text[1..-1]
+#     # semicolon_index = received_text.index(";")
 
-    #找不到分號就跳出
-    # return nil if semicolon_index.nil?
+#     #找不到分號就跳出
+#     # return nil if semicolon_index.nil?
 
-    # keyword = received_text[0..semicolon_index-1]
-    # message = received_text[semicolon_index+1..-1]
+#     # keyword = received_text[0..semicolon_index-1]
+#     # message = received_text[semicolon_index+1..-1]
 
-    #KeywordApping.create(keyword: keyword,message: message)
-    #將房號存入
+#     #KeywordApping.create(keyword: keyword,message: message)
+#     #將房號存入
 
-    RoomId.create(roomid: roomid)
-    '登入成功!請輸入彈幕'
-    return roomid
-end
+#     RoomId.create(roomid: roomid)
+#     '登入成功!請輸入彈幕'
+#     return roomid
+# end
 
 #將房號與收到的訊息帶入並存入
 def save_dangmoo(received_text)
@@ -51,11 +51,12 @@ def save_dangmoo(received_text)
 
     #如果開頭為";" 則跳出
     if received_text[0] == ';'
-        return nil
+        roomid = received_text[1..-1]
+        '登入成功!請輸入彈幕'
     else
         dangmoo = received_text;
     end
-    Dangmoo.create(room_id,dangmoo)
+    Dangmoo.create(roomid,dangmoo)
 end
 
 def channel_id
