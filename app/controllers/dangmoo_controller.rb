bdc92696e88b984f209684384c5f643a4df8c0cd
shinatatsu <shinatatsu040849@gmail.com>
2018-04-08 16:29:12 +0800

    use danmu

--- app/controllers/dangmoo_controller.rb
+++ app/controllers/dangmoo_controller.rb
@@ -104,4 +104,3 @@ def line
         config.channel_token = '2nuY4aNLo3XuxA7vQZyLaYnGg77cI5wRJj4IQq1KeB6jIbVGsEbDGDKxkQDjTpdgj524NnFhl4m0aAa3y3gjtE9UcYSceuLs7PYhV8OKLr6Mla+civUW1VGnPtuxxtAnTt/BiFXb/1KqWNQF8+zLDgdB04t89/1O/w1cDnyilFU='
     }
 end
-end
