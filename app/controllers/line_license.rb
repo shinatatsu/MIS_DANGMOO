7f06b624262932d837e76a5c9c85f5cb12f739f6
shinatatsu <shinatatsu040849@gmail.com>
2018-04-10 15:06:48 +0800

    update license

---
+++ app/controllers/line_license.rb
@@ -0,0 +1,4 @@
+module License
+    channel_secret = 'e55f897882eea3d7a0a3874600e1eeed'
+    channel_token = '2nuY4aNLo3XuxA7vQZyLaYnGg77cI5wRJj4IQq1KeB6jIbVGsEbDGDKxkQDjTpdgj524NnFhl4m0aAa3y3gjtE9UcYSceuLs7PYhV8OKLr6Mla+civUW1VGnPtuxxtAnTt/BiFXb/1KqWNQF8+zLDgdB04t89/1O/w1cDnyilFU='
+end
